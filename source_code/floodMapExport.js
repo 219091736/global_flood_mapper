2b247745d023ca4c28d46575a88b6c9e69fb31fa
219091736 <mkhontaglen1@gmail.com>
2024-09-20 00:31:22 +0200

    Update floodMapExport.js
    Export as GEOTIFF.

--- source_code/floodMapExport.js
+++ source_code/floodMapExport.js
@@ -1,34 +1,26 @@
-// This function will be called by the main script to download the flood map.
+// This function will be called by the main script to export the flood map as a GeoTIFF file via URL.
 
-// Define a function to smoothen the raster and export the shapefile
-var getFloodShpUrl = function(floodLayer, value, radius, aoi, cellSize, filename){
-  // Define a boxcar or low-pass kernel.
+var getFloodGeoTIFFUrl = function(floodLayer, value, radius, aoi, cellSize, filename) {
+  // Define a boxcar or low-pass kernel for smoothing.
   var boxcar = ee.Kernel.square({
     radius: radius, units: 'pixels', magnitude: 1
   });
   
   // Smoothen and threshold the binary flood raster
   var smooth_flood = floodLayer.eq(value).convolve(boxcar);
-  var smooth_flood_binary = smooth_flood.updateMask(smooth_flood.gt(0.5)).gt(0)
+  var smooth_flood_binary = smooth_flood.updateMask(smooth_flood.gt(0.5)).gt(0);
   
-  // Vectorise the binary flood raster
-  var vectors = smooth_flood_binary.reduceToVectors({
-    geometry: aoi,
-    crs: floodLayer.projection(),
-    scale: cellSize,
-    geometryType: 'polygon',
-    eightConnected: false,
-    labelProperty: 'zone',
-    maxPixels: 9e12
-    });
+  // Generate the download URL for the GeoTIFF
+  var geotiff_url = smooth_flood_binary.getDownloadURL({
+    name: filename,                      // Filename for the exported GeoTIFF
+    region: aoi,                         // Area of interest (AOI) geometry
+    scale: cellSize,                     // Spatial resolution (cell size in meters)
+    crs: floodLayer.projection(),        // Coordinate Reference System (CRS)
+    fileFormat: 'GeoTIFF'                // Export format set to GeoTIFF
+  });
   
-  // Convert the vector to feature collection
-  var flood_vector = ee.FeatureCollection(vectors);
-  
-  // print download url
-  var vector_url = flood_vector.getDownloadURL('kml', [], filename)
-  
-  return(vector_url)
+  // Return the download URL for the user to download the GeoTIFF directly
+  return geotiff_url;
 }
 
-exports.getFloodShpUrl = getFloodShpUrl;
+exports.getFloodGeoTIFFUrl = getFloodGeoTIFFUrl;
